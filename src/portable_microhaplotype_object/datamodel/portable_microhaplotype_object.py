08f1fc43f5ccc1b5ab672f806f3d1fc1168c7006
Nicholas Hathaway <nickjhathaway@gmail.com>
2024-07-15 08:22:09 -0700

    no message

--- src/portable_microhaplotype_object/datamodel/portable_microhaplotype_object.py
+++ src/portable_microhaplotype_object/datamodel/portable_microhaplotype_object.py
@@ -1,5 +1,5 @@
 # Auto generated from portable_microhaplotype_object.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-07-14T23:47:44
+# Generation date: 2024-07-15T01:46:00
 # Schema: portable-microhaplotype-object
 #
 # id: https://plasmogenepi.github.io/portable-microhaplotype-object
